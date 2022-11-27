37f8173c1b3e983b61bec927f214c743fa2eaa58
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2022-11-27 23:11:53 +0900

    [Gold V] Title: CCW, Time: 112 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
+
+		int ans = x1 * y2 + x2 * y3 + x3 * y1 - y1 * x2 - y2 * x3 - y3 * x1;
+		
+		if (ans == 0) {
+			System.out.println(0);
+		} else if (ans > 0) {
+			System.out.println(1);
+		} else {
+			System.out.println(-1);
+		}
+		sc.close();
+
+	}
+}
