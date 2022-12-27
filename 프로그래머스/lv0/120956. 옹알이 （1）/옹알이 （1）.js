c1224a65f5affda5bf739ebb5a5f264f097e3147
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2022-12-27 19:11:37 +0900

    [level 0] Title: 옹알이 (1), Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,10 @@
+function solution(babbling) {
+    let answer = 0;
+    babbling.forEach(element => {
+        if(element.split(/aya|ye|woo|ma/g).join("")===""){
+            answer++;
+        }
+    });
+
+    return answer;
+}
