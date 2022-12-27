15a08abc8d6fbc686aa4973d0a6df65bf533de95
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2022-12-27 18:20:17 +0900

    [Gold III] Title: 우주신과의 교감, Time: 544 ms, Memory: 35860 KB -BaekjoonHub

---
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] Parent;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		// 정점의 개수
+		int v = Integer.parseInt(st.nextToken());
+
+		// 이미 연결된 통로의 개수 => 유니온으로 받아주자.
+		int m = Integer.parseInt(st.nextToken());
+		Parent = new int[v + 1];
+		for (int i = 1; i <= v; i++) {
+			Parent[i] = i;
+		}
+
+		// 신들의 xy좌표를 받아주자. 정점 숫자가 1부터 시작이니까 비어있는 인스턴스 하나 넣어주자.
+		ArrayList<int[]> list = new ArrayList<>();
+		list.add(new int[] {});
+
+		for (int i = 1; i <= v; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			list.add(new int[] { x, y });
+		}
+
+		// 뒤따라 나오는 m개의 숫자들은 서로 유니온 시켜준다. 이미 연결되어 있으니 거리추가 필요 X
+		for (int i = 1; i <= m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			union(a, b);
+		}
+
+		// 각 점간의 거리를 계산한 다음에 Node의 형태로 치환해서 간선정보를 힙에 집어넣는다.
+		PriorityQueue<Node> PQ = new PriorityQueue<>(new Comparator<Node>() {
+			@Override
+			public int compare(Node e1, Node e2) {
+				// compare 함수 태생이 return 값이 int형으로 강제되다보니 실수값 연산에서
+                // 차이가 작아지면 재대로 정렬이 안되는 문제가 생기는데 이때는 그냥 깡으로
+                // 1000 곱하게 만들어서 부동소수점 문제를 해결하자.
+                return (int) ((e1.cost - e2.cost)*1000);
+			}
+		});
+
+		for (int i = 1; i < v; i++) {
+			for (int j = i + 1; j <= v; j++) {
+				int[] e1 = list.get(i);
+				int[] e2 = list.get(j);
+				PQ.add(new Node(i, j, distance(e1, e2)));
+			}
+		}
+
+		double min_cost = 0;
+		while (!PQ.isEmpty()) {
+			Node node = PQ.poll();
+			if (find(node.from) != find(node.to)) {
+				union(node.to, node.from);
+				min_cost += node.cost;
+			}
+		}
+
+		System.out.println(String.format("%.2f", min_cost));
+
+	}// main
+
+	static double distance(int[] e1, int[] e2) {
+		return Math.sqrt(Math.pow(e1[0] - e2[0], 2) + Math.pow(e1[1] - e2[1], 2));
+	}
+
+	static int find(int x) {
+		if (Parent[x] == x) {
+			return x;
+		} else {
+			return find(Parent[x]);
+		}
+	}// find
+
+	static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a > b) {
+			Parent[a] = b;
+		} else {
+			Parent[b] = a;
+		}
+	}// union
+
+	static class Node {
+		int from, to;
+		double cost;
+
+		public Node(int from, int to, double cost) {
+			this.from = from;
+			this.to = to;
+			this.cost = cost;
+		}
+
+	}// Node
+
+}
