e4dc88f93696a53f3037fc0755bf2f103194c30f
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2023-01-03 11:06:05 +0900

    [Gold V] Title: 직육면체, Time: 148 ms, Memory: 14724 KB -BaekjoonHub

---
+++ 백준/Gold/27087. 직육면체/직육면체.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (tc-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            if (a % p == 0 && b % p == 0) {
+                sb.append(1 + "\n");
+                continue;
+            }
+            if (a % p == 0 && c % p == 0) {
+                sb.append(1 + "\n");
+                continue;
+            }
+            if (b % p == 0 && c % p == 0) {
+                sb.append(1 + "\n");
+                continue;
+            }
+            sb.append(0 + "\n");
+        }
+        System.out.println(sb.toString());
+    }//
+}
