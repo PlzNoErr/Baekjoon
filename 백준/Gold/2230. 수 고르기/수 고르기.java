282b6148bcaf4aa96f9416bf6b3b83ca6b0c488c
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2023-03-20 09:16:04 +0900

    [Gold V] Title: 수 고르기, Time: 356 ms, Memory: 29316 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+
+        int i = 0, j = 0;
+        int ans = Integer.MAX_VALUE;
+       
+        while (i < N) {
+            if (arr[i] - arr[j] < M) {
+                i++;
+                continue;
+            }
+
+            if (arr[i] - arr[j] == M) {
+                ans = M;
+                break;
+            }
+
+            ans = Math.min(ans, arr[i] - arr[j]);
+            j++;
+        }
+
+        System.out.println(ans);
+    }
+}
