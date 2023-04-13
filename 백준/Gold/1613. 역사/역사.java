1f3a8eec8d5f43e01a88ce06fa55fc42761f5e97
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2023-04-13 09:35:14 +0900

    [Gold III] Title: 역사, Time: 712 ms, Memory: 39316 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.java
+++ 백준/Gold/1613. 역사/역사.java
@@ -27,7 +27,6 @@ public class Main {
         for (int k = 1; k <= v; k++) {
             for (int i = 1; i <= v; i++) {
                 for (int j = 1; j <= v; j++) {
-                    if (i == j) continue;
                     dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
