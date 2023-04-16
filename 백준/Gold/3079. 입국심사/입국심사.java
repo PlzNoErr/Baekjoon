fdabf44f30199630d1865e97e5a0cb362fa26713
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2023-04-16 22:40:36 +0900

    [Gold V] Title: 입국심사, Time: 320 ms, Memory: 23288 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -1,39 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int N, M;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] Arr = new int[n];
 
-        int max_value = 0;
-        for (int i = 0; i < n; i++) {
-            Arr[i] = Integer.parseInt(br.readLine());
-            max_value = Math.max(max_value, Arr[i]);
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        long maxTime = 0;
+        int [] times = new int[N];
+
+        for(int i = 0 ; i < N ; i++) {
+            times[i] = Integer.parseInt(br.readLine());
+            maxTime = Math.max(maxTime, times[i]);
         }
 
-        long low = 1L;
-        long high = (max_value) * 1000000000L;
+        long start = 0;
+        long end = maxTime * M;
 
-        while (low <= high) {
-            long mid = (low + high) / 2;
+        long ans = Long.MAX_VALUE;
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
             long cnt = 0;
-            for (int i = 0; i < n; i++) {
-                cnt += (mid / Arr[i]);
+
+            for (int i = 0 ; i < N ; i++) {
+                if (cnt > M) break; 
+                cnt += mid / times[i];
             }
-            if (cnt >= m) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+
+            if (cnt < M) start = mid + 1;
+            else {
+                ans = Math.min(ans, mid);
+                end = mid - 1;
             }
         }
 
-        System.out.println(low);
+        System.out.println(ans);
     }
 }
