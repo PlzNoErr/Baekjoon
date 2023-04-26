7477715abbd675c6d21b0478533c8f7a0366a565
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2023-04-26 09:28:09 +0900

    [Gold V] Title: 끝나지 않는 파티, Time: 456 ms, Memory: 26904 KB -BaekjoonHub

---
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int tc = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) map[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (tc-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            if (map[from][to] <= cost) sb.append("Enjoy other party\n");
+            else sb.append("Stay here\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
