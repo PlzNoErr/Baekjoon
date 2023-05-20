2b5e59364de43929c1854ea637454312f126e61d
YeonHak Lee <108788547+PlzNoErr@users.noreply.github.com>
2023-05-20 18:49:30 +0900

    [Gold V] Title: 등차 수열과 등비 수열, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/14277. 등차 수열과 등비 수열/등차 수열과 등비 수열.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        long d = Long.parseLong(st.nextToken());
+        long u = Long.parseLong(st.nextToken());
+
+        long count = 0;
+        if (a <= u) count = (u - a) / b + 1;
+
+        if (d == 1 && c <= u) {
+            if (a != c && (a >= c || (c - a) % b != 0)) count++;
+        } else {
+            while (c <= u) {
+                if (c < a || (c - a) % b != 0) count++;
+                c *= d;
+            }
+        }
+        System.out.println(count);
+    }
+}
